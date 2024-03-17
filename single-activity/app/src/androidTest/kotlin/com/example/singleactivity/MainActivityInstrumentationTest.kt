239bd0baac414585b94f556e605d35dc365353ea
Carmen Alvarez <carmen@rmen.ca>
2024-03-17 17:58:45 +0100

    Step 1.2 - Add single-activity UI instrumentation test

---
+++ single-activity/app/src/androidTest/kotlin/com/example/singleactivity/MainActivityInstrumentationTest.kt
@@ -0,0 +1,25 @@
+package com.example.singleactivity
+
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityInstrumentationTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun testMainActivity() {
+        composeTestRule.onNodeWithTag("ResultText").assertTextEquals("0")
+        composeTestRule.onNode(hasText("Add one")).performClick()
+        composeTestRule.onNodeWithTag("ResultText").assertTextEquals("1")
+    }
+}
