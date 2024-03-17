72fa2c303fc5f95eadc5ab17fd3b8d34b47da6fb
Carmen Alvarez <carmen@rmen.ca>
2024-03-17 23:32:09 +0100

    Step 2.6 - Create a multi-activity shared test to run on either robolectric or as an instrumentation test.

---
+++ multi-activity/app/src/sharedTest/kotlin/com/example/multiactivity/MultiActivitySharedTest.kt
@@ -0,0 +1,39 @@
+package com.example.multiactivity
+
+
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/*
+ * This test can be run on the jvm, or as an instrumented test.
+ *
+ * The gradle tasks for this are:
+ *
+ * Jvm:
+ * :app:testDebugUnitTest --tests "com.example.multiactivity.MultiActivitySharedTest"
+ *
+ * Instrumented test:
+ * :app:connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.class="com.example.multiactivity.MultiActivitySharedTest"
+ */
+@RunWith(AndroidJUnit4::class)
+class MultiActivitySharedTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun testMainActivity() {
+        composeTestRule.onNode(hasText("Click me")).performClick()
+        composeTestRule.onNodeWithTag("TextInput").performTextInput("test text")
+        composeTestRule.onNode(hasText("Close")).performClick()
+        composeTestRule.onNodeWithTag("ResultText").assertTextEquals("test text")
+    }
+}
