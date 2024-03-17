a58a2e2bf217420e90d80eaad7385f2f72e47146
Carmen Alvarez <carmen@rmen.ca>
2024-03-17 19:00:35 +0100

    Step 1.4 - Adapt the single-activity Robolectric test to not use Roblectric apis.

--- single-activity/app/src/test/kotlin/com/example/singleactivity/MainActivityRobolectricTest.kt
+++ single-activity/app/src/test/kotlin/com/example/singleactivity/MainActivityRobolectricTest.kt
@@ -2,29 +2,24 @@ package com.example.singleactivity
 
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.hasText
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.Robolectric
-import org.robolectric.RobolectricTestRunner
 
-@RunWith(RobolectricTestRunner::class)
+@RunWith(AndroidJUnit4::class)
 class MainActivityRobolectricTest {
 
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
     fun testMainActivity() {
-        // Use Robolectric apis to build the MainActivity
-        Robolectric.buildActivity(MainActivity::class.java).use { controllerMainActivity ->
-            controllerMainActivity.setup() // Moves Activity to RESUMED state
-            composeTestRule.onNodeWithTag("ResultText").assertTextEquals("0")
-            composeTestRule.onNode(hasText("Add one")).performClick()
-            composeTestRule.onNodeWithTag("ResultText").assertTextEquals("1")
-        }
+        composeTestRule.onNodeWithTag("ResultText").assertTextEquals("0")
+        composeTestRule.onNode(hasText("Add one")).performClick()
+        composeTestRule.onNodeWithTag("ResultText").assertTextEquals("1")
     }
 }
