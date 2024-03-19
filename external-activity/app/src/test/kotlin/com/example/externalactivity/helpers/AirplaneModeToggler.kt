ea5ba11853a88d2c68effe6bc13485e04f43a579
Carmen Alvarez <carmen@rmen.ca>
2024-03-19 21:18:52 +0100

    Step 3.3 - Add external-activity robolectric test

---
+++ external-activity/app/src/test/kotlin/com/example/externalactivity/helpers/AirplaneModeToggler.kt
@@ -0,0 +1,15 @@
+package com.example.externalactivity.helpers
+
+import android.content.Context
+import android.content.Intent
+import androidx.test.core.app.ApplicationProvider
+import org.robolectric.shadows.ShadowSettings
+
+class AirplaneModeToggler {
+    fun setAirplaneMode(value: Boolean) {
+        ShadowSettings.setAirplaneMode(value)
+        ApplicationProvider.getApplicationContext<Context>().sendBroadcast(
+            Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        )
+    }
+}
