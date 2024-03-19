f57842c10a7bd2a5c07dc2663e7145fab3edcf6a
Carmen Alvarez <carmen@rmen.ca>
2024-03-19 21:21:57 +0100

    Step 3.4 - Create an external-activity shared test to run on either robolectric or as an instrumentation test.

---
+++ external-activity/app/src/sharedTest/kotlin/com/example/externalactivity/MainActivitySharedTest.kt
@@ -0,0 +1,47 @@
+package com.example.externalactivity
+
+
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.externalactivity.helpers.TestHelpers
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/*
+ * This test can be run on the jvm, or as an instrumented test.
+ *
+ * The gradle tasks for this are:
+ *
+ * Jvm:
+ * :app:testDebugUnitTest --tests "com.example.externalactivity.MainActivitySharedTest"
+ *
+ * Instrumented test:
+ * :app:connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.class="com.example.externalactivity.MainActivitySharedTest"
+ */
+@RunWith(AndroidJUnit4::class)
+class MainActivitySharedTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    private val helpers = TestHelpers()
+
+    @Test
+    fun testMainActivity() {
+        // Toggle airplane mode on
+        helpers.setAirplaneMode(true)
+        helpers.goBackToMainActivity()
+        // Check our activity's text
+        composeTestRule.onNodeWithTag("ResultText").assertTextEquals("Airplane ON")
+
+        // Toggle airplane mode off
+        helpers.setAirplaneMode(false)
+        helpers.goBackToMainActivity()
+        // Check our activity's text
+        composeTestRule.onNodeWithTag("ResultText").assertTextEquals("Airplane OFF")
+    }
+
+}
