c021dccd2115e917e5c6f003ab7c7b48153f0699
Carmen Alvarez <carmen@rmen.ca>
2024-03-19 21:12:42 +0100

    Step 3.2 - Add external-activity UI instrumentation test

---
+++ external-activity/app/src/androidTest/kotlin/com/example/externalactivity/ExternalActivityInstrumentationTest.kt
@@ -0,0 +1,36 @@
+package com.example.externalactivity
+
+
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.externalactivity.helpers.TestHelpers
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ExternalActivityInstrumentationTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    private val helpers = TestHelpers()
+
+    @Test
+    fun testMainActivity() {
+        // Toggle airplane mode on
+        helpers.setAirplaneMode(true)
+        helpers.goBackToMainActivity()
+        // Check our activity's text
+        composeTestRule.onNodeWithTag("ResultText").assertTextEquals("Airplane ON")
+
+        // Toggle airplane mode off
+        helpers.setAirplaneMode(false)
+        helpers.goBackToMainActivity()
+        // Check our activity's text
+        composeTestRule.onNodeWithTag("ResultText").assertTextEquals("Airplane OFF")
+    }
+
+}
